faaa051cc3c4424639afbf08e80eb03404aacd88
Mikhail Chirkov <mchirkov@ivi.ru>
2021-02-15 10:01:06 +0300

    Shift to setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/venv python
+from distutils.core import setup
+
+setup(name='Windows, Iframe, Alerts',
+      version='1.0',
+      description='Repo for lesson about windows and iframes',
+      author='Mikhail C.',
+      author_email='konflic@mail.ru',
+      install_requires=[
+          "pytest==6.2.2",
+          "selenium==3.141.0",
+          "ipython=7.7.0"
+      ]
+      )
